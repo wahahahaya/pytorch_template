dadac043defeda7e85553ad85bf07cfdc5ae3d6a
Tsao-Lun, Chen <31922920+wahahahaya@users.noreply.github.com>
2021-12-08 16:27:58 +0800

    Add files via upload

---
+++ util/utils.py
@@ -0,0 +1,13 @@
+import json
+from collections import OrderedDict
+from pathlib import Path
+
+def read_json(fname):
+    fname = Path(fname)
+    with fname.open('rt') as handle:
+        return json.load(handle, object_hook=OrderedDict)
+
+def write_json(content, fname):
+    fname = Path(fname)
+    with fname.open('wt') as handle:
+        json.dump(content, handle, indent=4, sort_keys=False)
